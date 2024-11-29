ecbf57353905b2073f3fe25939c9576172ac63bb
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2024-11-29 18:36:42 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,9 +2,16 @@ class Config {
   constructor() {
     this.baseURL = 'https://nodepay.org';
     this.ipCheckURL = 'https://ipinfo.io/json';
-    this.pingURL = 'http://13.215.134.222/api/network/ping';
+    this.pingURL = [
+      'http://nodepaypantek.dayon.me/api/network/ping',
+      'http://13.215.134.222/api/network/ping',
+      'http://18.139.20.49/api/network/ping',
+      'http://52.74.35.173/api/network/ping',
+      'http://52.77.10.116/api/network/ping',
+      'http://3.1.154.253/api/network/ping',
+    ];
     this.retryInterval = 30000;
-    this.sessionURL = 'http://18.136.143.169/api/auth/session';
+    this.sessionURL = 'http://api.nodepay.ai/api/auth/session';
   }
 }
 
